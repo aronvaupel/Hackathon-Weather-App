b641ba82f9cce742d8bbbbd1af81822812b24ee9
Aron Vaupel <aronvaupel@Arons-MacBook-Pro.local>
2022-03-16 08:26:56 +0100

    new pic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 const log = console.log;
 
 import { GeocoderAutocomplete } from "@geoapify/geocoder-autocomplete";
-const GEO_KEY = "a7242c3efe674cc5842fca4734ee575f" || ""; //process.env.GEO_KEY//
+
+
+const locationElement = document.querySelector('[data-location]') as HTMLElement;
+const statusElement = document.querySelector('[data-status]') as HTMLElement;
+const temperatureElement = document.querySelector('[data-temperature]') as HTMLElement;
+const precipitationElement = document.querySelector('[data-percipitation]') as HTMLElement;
+const windElement = document.querySelector('[data-wind]') as HTMLElement;
+
+const GEO_KEY = "ba21a0d4fde441b1a9dceb2df1ead4f1" || ""; //process.env.GEO_KEY//
+
+function setWeatherData(data:any){ //, place: string) {
+  // locationElement.textContent = place;
+  statusElement.textContent = data.summary; // weather status from API
+  temperatureElement.textContent = data.temperature;
+  precipitationElement.textContent = `${data.precipProbability * 100}%`
+  windElement.textContent = data.windSpeed
+}
+
 
 const autocomplete = new GeocoderAutocomplete(
   document.getElementById("autocomplete") as HTMLElement,
@@ -11,27 +28,38 @@ const autocomplete = new GeocoderAutocomplete(
 let latitude: number | null = 0;
 let longitude: number | null = 0;
 
-autocomplete.on("select", (location) => {
+ autocomplete.on("select", (location) => {
   if (location === null) return;
-  latitude = location.geometry.coordinates[0][0][0];
-  longitude = location.geometry.coordinates[0][0][1];
+   longitude = location.geometry.coordinates[0][0][0];
+   latitude = location.geometry.coordinates[0][0][1];
+
+  const data = {
+    lat: latitude,
+    lon: longitude
+  }
 
   log("latitude", latitude);
   log("longitude", longitude);
-  fetch("http://localhost:3001/weather", {
+
+  sendPost(data)
+ 
+})
+
+
+async function sendPost(data: any) {
+  log(data)
+   await fetch("http://localhost:3001/weather", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      latitude: latitude,
-      longitude: longitude,
-    }),
+    body: JSON.stringify(data),
+  }).then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
   })
-    .then((res) => res.json())
-    .then((data) => {
-      log(data);
-      // setWeatherData(data, place.formatted_address);
-    });
-});
+  .catch((error) => {
+    console.error('Err:', error);
+  });
+}
